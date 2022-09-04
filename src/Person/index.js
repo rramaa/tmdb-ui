8b26f43f6fb1c006873ab287869565d9eafde40d
Ramakrishna <ramakrishna@mindtickle.com>
2022-09-04 23:44:05 +0530

    major features implemented

--- src/Person/index.js
+++ src/Person/index.js
@@ -1,11 +1,13 @@
 import {useParams} from "react-router-dom";
 import Image from "../Image";
 import {useFetcher} from "../Fetcher";
+import Carousel from "../Carousel";
 
 export default function MovieDetails() {
     let {id} = useParams()
     let fetchApi = useFetcher()
     let {data, loading} = fetchApi(`https://api.themoviedb.org/3/person/${id}`)
+    let {data: credits, loading: creditsLoading} = fetchApi(`https://api.themoviedb.org/3/person/${id}/combined_credits`, {clientOnly: true})
     if(loading) {
         return "Loading"
     }
@@ -15,9 +17,21 @@ export default function MovieDetails() {
             <div className={"details-overview"}>
                 <h2>{data.name}</h2>
                 <button>Add to wishlist</button>
-                <h3>{data.known_for_department}</h3>
+                <h3>Known for {data.known_for_department}</h3>
             </div>
         </div>
+        {!creditsLoading && credits?.cast.length > 0 && <Carousel
+            baseRoute={"/movie"}
+            title={"Credits"}
+            items={credits?.cast.map((v) => {
+                return {
+                    image: v.backdrop_path,
+                    id: v.id,
+                    title: v.original_title,
+                    type: v.media_type
+                }
+            })}
+        />}
     </div>
 }
 
