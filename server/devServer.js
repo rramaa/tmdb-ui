b0ba6e9a1a8736504ca371261e2321c64c5ad1cc
Ramakrishna <ramakrishna@mindtickle.com>
2022-09-07 13:49:50 +0530

    added some error handling

--- server/devServer.js
+++ server/devServer.js
@@ -2,9 +2,19 @@ import "@babel/register";
 import webpackConfig from "../webpack.config";
 import { create, start } from "./";
 import "dotenv";
-import path from "path";
+import URL from "url";
+import fetch from "node-fetch-commonjs"
 
-const manifest = require(path.join(webpackConfig.output.path, "manifest.json"));
+const manifestFileUrl = URL.resolve(webpackConfig.output.publicPath, "manifest.json")
 
-create({ manifest, entry: webpackConfig.entry });
-start();
+fetch(manifestFileUrl)
+    .then(res => res.json())
+    .then(manifest => {
+        create({ manifest, entry: webpackConfig.entry });
+        start();
+    })
+    .catch(e => {
+        console.log(`Could not read the manifest file which was expected to be found at ${manifestFileUrl}`)
+        console.log("make sure your webpack build is running for client application and the public path is reachable")
+        throw e
+    });
